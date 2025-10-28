e2bf53fd6b88e0c0f2c343a5130add50ff304bd5
SandraMartinez4 <sandraallendemartinez1456@gmail.com>
2025-10-28 13:11:06 -0600

    sandyProyect22

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,24 +1,24 @@
-# views.py
+
+    # views.py
 
 from django.shortcuts import render
-# Solo importamos lo necesario
 from .nsl_kdd_loader import cargar_dataset 
 
 def home(request):
     df, atributos = cargar_dataset()
     
-    # Definir la cantidad de filas a mostrar (20 filas)
+    # Cantidad de filas a mostrar (20, como solicitaste anteriormente)
     NUM_FILAS_A_MOSTRAR = 20  
 
-    # Preparar las variables para la tabla dinámica
+    # Preparar la tabla
     column_headers = atributos 
-    
-    # Obtiene los VALORES de las primeras 20 filas como una lista de listas
     data_table = df.head(NUM_FILAS_A_MOSTRAR).values.tolist()
     
-    # Renderizar con las variables necesarias para la tabla
+    # ----------------------------------------------------
+    # CAMBIO: Asegurarse de pasar 'atributos' a la plantilla
+    # ----------------------------------------------------
     return render(request, "dashboard/home.html", {
-        "column_headers": column_headers, # Encabezados de la tabla
-        "data_table": data_table,        # Datos de las 20 filas
-        # Ya no se pasa la variable 'atributos' ni nada de gráficas
+        "atributos": atributos,          # <--- VARIABLE REINTRODUCIDA
+        "column_headers": column_headers,
+        "data_table": data_table,        
     })
